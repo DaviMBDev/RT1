24f4870a879c3cc36fe09d515e950ab892312589
DaviMBDev <davimb00789@gmail.com>
2025-03-13 12:11:48 -0300

    Começando

---
+++ out/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var endereco_1 = __importDefault(require("./endereco"));
+var end = new endereco_1.default();
---
+++ src/empresa.ts
@@ -0,0 +1,9 @@
+class Empresa{
+    public razaoSocial: string
+    public nomeFantasia: string
+    public cnpj: string
+    public endereco: string
+
+    
+
+}
